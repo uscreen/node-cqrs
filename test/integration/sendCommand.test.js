1bec42858d8da035f82f36312ddd6b2b8716f483
Marcus Spiegel <spiegel@uscreen.de>
2019-10-09 16:27:49 +0200

    save work

--- test/integration/sendCommand.test.js
+++ test/integration/sendCommand.test.js
@@ -1,14 +1,17 @@
 const tap = require('tap')
-const { MongoClient } = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb')
+const { Container, MongoEventStorage } = require('../../index')
 const { config } = require('../helper')
 
 tap.test('Mongo-Connection', async t => {
   const client = await MongoClient.connect(config.mongoUri)
   const db = client.db()
   const collection = db.collection('insert-test')
+  const events = db.collection('events-test-1')
 
   try {
     await collection.drop()
+    await events.drop()
   } catch (_) {}
 
   const result = await collection.insertOne({ a: 1 })
@@ -17,6 +20,13 @@ tap.test('Mongo-Connection', async t => {
   /**
    * -------
    */
+
+  const cqrs = new Container()
+
+  cqrs.register(MongoEventStorage, 'storage')
+  cqrs.registerInstance({ events, ObjectId }, 'MongoEventStorageConfig')
+
+  console.log(cqrs)
 
   client.close()
   t.end()
