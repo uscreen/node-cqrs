75b9a611f946eb24d16bdec025c9f490de4eef79
Marcus Spiegel <spiegel@uscreen.de>
2019-10-09 15:31:04 +0200

    add test config helper

--- test/helper.js
+++ test/helper.js
@@ -1,3 +1,27 @@
 'use strict'
 
-module.exports = {}
+const envSchema = require('env-schema')
+
+const schema = {
+  type: 'object',
+  properties: {
+    mongoServer: {
+      default: '127.0.0.1:27017'
+    }
+  }
+}
+
+const config = envSchema({
+  schema: schema,
+  dotenv: true
+})
+
+const database = process.env.TAP
+  ? `cqrs-kit-test-${process.env.TAP_CHILD_ID}`
+  : 'cqrs-kit-test'
+
+config.mongoUri = `mongodb://${config.mongoServer}/${database}`
+
+module.exports = {
+  config
+}
--- test/noop.test.js
+++ test/noop.test.js
@@ -1,6 +1,8 @@
 const tap = require('tap')
+const { config } = require('./helper')
 
 tap.test('Test Setup', t => {
+  console.log(config)
   t.strictEqual(true, true, 'Tests and assertions should work')
   t.end()
 })
